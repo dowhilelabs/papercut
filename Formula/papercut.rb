a9e3bbd52e7ca64561f87e223e92cc187f5adfd6
Joeseph Rodrigues <dowhilegeek@gmail.com>
2026-08-04 16:54:25 -0700

    formula: fix url to v0.1.5 (was pointing at v0.1.4 tarball)

--- Formula/papercut.rb
+++ Formula/papercut.rb
@@ -2,8 +2,8 @@ class Papercut < Formula
   desc "A tiny CLI that gives AI agents a complaint box."
   homepage "https://github.com/dowhilelabs/papercut"
   version "0.1.5"
-  url "https://github.com/dowhilelabs/papercut/releases/download/v0.1.4/papercut-aarch64-apple-darwin.tar.gz"
-  # SHA-256 of papercut-aarch64-apple-darwin.tar.gz (v0.1.4 release).
+  url "https://github.com/dowhilelabs/papercut/releases/download/v0.1.5/papercut-aarch64-apple-darwin.tar.gz"
+  # SHA-256 of papercut-aarch64-apple-darwin.tar.gz (v0.1.5 release).
   sha256 "eb7ee26df5e0d01ff77178b268d1d2e80f54210623e1c8169639d23081ea35c8"
 
   depends_on :macos
