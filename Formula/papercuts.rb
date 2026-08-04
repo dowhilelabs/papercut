2608c0c944afc4c6a5552dec46845de380855df1
Joeseph Rodrigues <dowhilegeek@gmail.com>
2026-08-04 14:48:03 -0700

    Fill real v0.1.0 release SHA256 into brew formula

--- Formula/papercuts.rb
+++ Formula/papercuts.rb
@@ -3,8 +3,8 @@ class Papercuts < Formula
   homepage "https://github.com/dowhilelabs/papercut"
   version "0.1.0"
   url "https://github.com/dowhilelabs/papercut/releases/download/v0.1.0/papercuts-aarch64-apple-darwin.tar.gz"
-  # Replace with the SHA-256 from the release's `papercuts-aarch64-apple-darwin.sha256`.
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  # SHA-256 of papercuts-aarch64-apple-darwin.tar.gz (v0.1.0 release).
+  sha256 "362511afdd37fcabd144346d9929ebe9ac5f3dec49bb28eb4622e164fd168900"
 
   depends_on :macos
 
